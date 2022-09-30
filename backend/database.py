4512db67c89fd45b4f70c9418c8c60cd492b2490
Evgeniia <57112057+Zhenia-Magic@users.noreply.github.com>
2022-09-30 12:21:45 -0300

    added database declarations

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, Table
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -6,13 +6,23 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    username = Column(String)
+    hashed_password = Column(String)
+    first_name = Column(String)
+    is_admin = Column(Boolean)
+
+
+class Tag(Base):
+    __tablename__ = 'tags'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    fullname = Column(String)
-    nickname = Column(String)
-
+    description = Column(String)
+    deleted = Column(Boolean)
 
 # parent_id = Column(Integer, ForeignKey("parent.id"))
 # children = relationship("Child", backref="parent")
--- pages/users.py
+++ pages/users.py
@@ -1,13 +1,14 @@
 import streamlit as st
 from app import user_store
+import re
 from backend.storage import LOGGED_IN, INCORRECT_PASSWORD, NO_USER_FOUND
-from models import User
-from storage import LOGGED_IN, INCORRECT_PASSWORD, NO_USER_FOUND
+from backend.models import User
 
 st.title("Users")
 
 user_option = st.radio("Do you want to log in or sign up?",
                        ("Log In", "Sign Up"), index=0)
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
 if user_option == "Log In":
     st.subheader("Log In Form")
@@ -26,23 +27,43 @@ if user_option == "Log In":
             st.write("Password is not correct.")
 
 elif user_option == "Sign Up":
-    invalid_input = False
+    invalid_input_pass = False
+    invalid_input_conf_pass = False
+    invalid_input_email = False
+
     st.subheader("Sing Up Form")
     email = st.text_input("Email")
+
+    if re.fullmatch(regex, email) is None:
+        invalid_input_email = True
+        st.error("Email is not valid")
+    else:
+        invalid_input_email = False
+
     username = st.text_input('Username')
     first_name = st.text_input('First name')
     last_name = st.text_input('Last name')
     password = st.text_input("Create a password", type="password")
+
     if len(password) < 8:
-        invalid_input = True
+        invalid_input_pass = True
         st.error("Password must be longer than 8 symbols")
     else:
-        invalid_input = False
+        invalid_input_pass = False
+
     conf_password = st.text_input("Confirm a password", type="password")
-    if invalid_input:
+
+    if password != conf_password:
+        invalid_input_conf_pass = True
+        st.error("Passwords are not the same")
+    else:
+        invalid_input_conf_pass = False
+
+    if any([invalid_input_pass, invalid_input_conf_pass, invalid_input_email]):
         sign_up_button = st.button("Sign Up", disabled=True)
     else:
         sign_up_button = st.button("Sign Up")
+
     if sign_up_button:
         if password != conf_password:
             st.write('Passwords do not match')
